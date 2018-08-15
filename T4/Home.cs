a0df2dd203461d9574fe97616a1358eb05511746
eeroom <eedroom@163.com>
2018-08-15 10:05:15 +0800

    2018年8月15日，适配mysql

--- T4/Home.cs
+++ T4/Home.cs
@@ -12,9 +12,22 @@ namespace T4
             var cnnstr= System.Configuration.ConfigurationManager.ConnectionStrings["mysqlmaster"].ConnectionString;
             Azeroth.Nalu.DbContextMysql dbContext = new Azeroth.Nalu.DbContextMysql() {  Cnnstr=cnnstr};
             var query= dbContext.CreateQuery();
-            var tbEvaPeople= query.Set<Tb_EvaluatedPeople>();
-            query.Select(tbEvaPeople.Cols());
-            var lst= query.ToList<Tb_EvaluatedPeople>();
+            var dbset= query.Set<Tb_ArticleInfo>();
+            query.Select(dbset.Cols());
+            var lst= query.ToList<Tb_ArticleInfo>();
+
+            using (var cnn=new MySql.Data.MySqlClient.MySqlConnection(cnnstr))
+            {
+                cnn.Open();
+                using (var cmd=cnn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Tb_ArticleInfo";
+                    using (var reader=cmd.ExecuteReader( System.Data.CommandBehavior.SchemaOnly))
+                    {
+                        var table= reader.GetSchemaTable();
+                    }
+                }
+            }
 
         }
 
