2b26e753d1fc7469740784db1063a4ec6efceee0
邓常青 <eedroom@163.com>
2021-07-26 00:01:46 +0800

    ----------

---
+++ EntityframworkMssqlserver/T4/DbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Model
+{
+  public partial class DbContext : System.Data.Entity.DbContext
+  {
+        public DbContext() : base("name=hz")
+        {
+        }    
+    /// <summary>
+    /// 
+    /// <summary>
+    public virtual DbSet<Log> Log { get; set; }
+    }
+}
